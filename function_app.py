6b36a74f7173b0a73f48b80611b317c43d9d31ed
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-03-27 15:49:13 +0100

    Removed client credentials and secrets

--- function_app.py
+++ function_app.py
@@ -1,15 +1,8 @@
 import azure.functions as func
-from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
 from src import ddc
 from src import cve
 
-credential = DefaultAzureCredential()
-
-KEY_VAULT_NAME = "dae1041-soc-kv001"
-KEY_VAULT_URI = f"https://{KEY_VAULT_NAME}.vault.azure.net"
-client = SecretClient(vault_url=KEY_VAULT_URI, credential=credential)
-
 app = func.FunctionApp()
+
 app.register_blueprint(ddc.bp)
 app.register_blueprint(cve.bp)
