2689976bb532f7a1c2ebb6b3fbec3f1923b0d59b
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-03-27 23:19:27 +0100

    Code refactor

--- function_app.py
+++ function_app.py
@@ -1,8 +1,8 @@
 import azure.functions as func
-# from src import ddc
+from src import ddc
 from src import cve
 
 app = func.FunctionApp()
 
-# app.register_blueprint(ddc.bp)
+app.register_blueprint(ddc.bp)
 app.register_blueprint(cve.bp)
