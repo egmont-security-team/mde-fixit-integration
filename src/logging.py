66d83af8e9a1d20e35fe514906fafd0d28498ade
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-03-29 21:35:21 +0100

    Added propper connection string env

--- src/logging.py
+++ src/logging.py
@@ -5,7 +5,12 @@ from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 def init_logger():
     logger = logging.getLogger(__name__)
-    instrumentation_key = os.getenv("APPINSIGHTS_INSTRUMENTATIONKEY")
+    instrumentation_key = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
+
+    if not instrumentation_key:
+        logger.error(
+            'Couldn\'t setup Application Insights logging since "APPLICATIONINSIGHTS_CONNECTION_STRING" is not present.'
+        )
 
     if not logger.handlers:
         azure_handler = AzureLogHandler(
