81572c2a48ff1a2b936de1400693dbbd43262921
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-03-29 21:45:01 +0100

    Renamed variable to proper name

--- src/logging.py
+++ src/logging.py
@@ -6,16 +6,16 @@ from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 def init_logger():
     logger = logging.getLogger(__name__)
-    instrumentation_key = os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+    connection_string = os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
 
-    if not instrumentation_key:
+    if not connection_string:
         logger.error(
             'Couldn\'t setup Application Insights logging since "APPLICATIONINSIGHTS_CONNECTION_STRING" is not present.'
         )
 
     if not logger.handlers:
         azure_handler = AzureLogHandler(
-            connection_string=instrumentation_key
+            connection_string=connection_string
         )
         logger.addHandler(azure_handler)
 
