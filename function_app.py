8027ac6ddfa2e7f17f5c2be46e8bf924c3af1627
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-07-27 02:35:22 +0200

    Fixed logging

--- function_app.py
+++ function_app.py
@@ -1,9 +1,20 @@
+import logging
+
 import azure.functions as func
 from azure.monitor.opentelemetry import configure_azure_monitor
 
 from src import cve, ddc2, ddc3
 
 configure_azure_monitor()
+# This is a workaround for the issue where the optentelemetry
+# integration logs unwanted messages to the console.
+# https://github.com/Azure/azure-functions-python-worker/issues/1342
+logging.getLogger("azure.core.pipeline.policies.http_logging_policy").setLevel(
+    logging.WARNING
+)
+logging.getLogger("azure.monitor.opentelemetry.exporter.export").setLevel(
+    logging.WARNING
+)
 
 app = func.FunctionApp()
 
