2ab246e84094e4cd930b0c61a3d13cf1fb2c3221
Kasper Jønsson <daekjo@egmont.com>
2024-08-01 09:45:39 +0200

    Local logging not sent to app insights

--- function_app.py
+++ function_app.py
@@ -1,20 +1,25 @@
 import logging
+import os
 
 import azure.functions as func
 from azure.monitor.opentelemetry import configure_azure_monitor
 
 from src import cve, ddc2, ddc3
 
-configure_azure_monitor()
-# This is a workaround for the issue where the optentelemetry
-# integration logs unwanted messages to the console.
-# https://github.com/Azure/azure-functions-python-worker/issues/1342
-logging.getLogger("azure.core.pipeline.policies.http_logging_policy").setLevel(
-    logging.WARNING
-)
-logging.getLogger("azure.monitor.opentelemetry.exporter.export").setLevel(
-    logging.WARNING
-)
+if conn := os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
+    configure_azure_monitor(connection_string=conn)
+    # This is a workaround for the issue where the opentelemetry
+    # integration, logs unwanted and duplicate messages.
+    # https://github.com/Azure/azure-functions-python-worker/issues/1342
+
+    # Stop duplicate logs
+    logging.getLogger("azure.core.pipeline.policies.http_logging_policy").setLevel(
+        logging.CRITICAL
+    )
+    # Stop unwanted logs from the exporter
+    logging.getLogger("azure.monitor.opentelemetry.exporter.export").setLevel(
+        logging.WARNING
+    )
 
 app = func.FunctionApp()
 
