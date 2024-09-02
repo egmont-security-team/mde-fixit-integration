70ef853ce6b3bc2672a315e1ea7d6b0bea7e4692
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-09-02 22:01:49 +0200

    Added back default logger

--- function_app.py
+++ function_app.py
@@ -2,7 +2,6 @@
 This is the entry point for the Azure Function App.
 """
 
-import logging
 import os
 
 import azure.functions as func
@@ -14,10 +13,6 @@ if conn := os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
     # This is a workaround for the issue where the root logger creates duplicate and unwanted logs.
     # This should be removed when the issue is fixed in the future.
     # https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/monitor/azure-monitor-opentelemetry#logging-issues
-    root_logger = logging.getLogger()
-    for handler in root_logger.handlers[:]:
-        root_logger.removeHandler(handler)
-
     configure_azure_monitor(connection_string=conn)
 
 app = func.FunctionApp()
