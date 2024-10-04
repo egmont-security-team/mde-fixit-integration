0552145a4710fad4185b1aedc20bcf84953c728a
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-10-04 10:53:03 +0200

    Made Terraform ignore threshold settings

--- terraform/app.tf
+++ terraform/app.tf
@@ -113,6 +113,8 @@ resource "azurerm_linux_function_app" "app" {
     ignore_changes = [
       tags,
       app_settings["WEBSITE_RUN_FROM_PACKAGE"],
+      app_settings["CVE_PC_THRESHOLD"],
+      app_settings["CVE_SERVER_THRESHOLD"],
     ]
   }
 }
@@ -170,6 +172,8 @@ resource "azurerm_linux_function_app_slot" "stag" {
     ignore_changes = [
       tags,
       app_settings["WEBSITE_RUN_FROM_PACKAGE"],
+      app_settings["CVE_PC_THRESHOLD"],
+      app_settings["CVE_SERVER_THRESHOLD"],
     ]
   }
 }
